4828ce4db3e93aee0c545c98277caa58bc7970bb
Jim Sullivan <jimpatricksullivan@gmail.com>
2014-06-22 16:37:03 -0400

    Added a Foundation Reveal, just for fun.

--- js/views/SomeView.js
+++ js/views/SomeView.js
@@ -1,8 +1,9 @@
 /*global define */
 define([
     'backbone',
+    'foundation.reveal',
     'hbs!templates/someTemplate'
-], function (Backbone, Template) {
+], function (Backbone, reveal, Template) {
     'use strict';
 
     return Backbone.View.extend({
@@ -10,6 +11,8 @@ define([
             this.$el.html(Template({
                 thing: 'World'
             }));
+            this.$el.foundation();
+            this.$('#helloWorld').foundation('reveal', 'open');
         }
     })
 });
