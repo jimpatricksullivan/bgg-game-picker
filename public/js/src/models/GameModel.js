a9defe8301fea384c9eb043cc3cba74296e56e3d
Jim Sullivan <jimpatricksullivan@gmail.com>
2014-12-25 12:06:31 -0500

    Get collection fetching from endpoint
    Now that this app has an endpoint which proxies and simplifies bgg's
     endpoint, just fetch from that.

--- public/js/src/models/GameModel.js
+++ public/js/src/models/GameModel.js
@@ -6,27 +6,6 @@ define([
 
     return Backbone.Model.extend({
 
-        /**
-         * Parse the json object returned by the server, which is bonkers because it's a just a rudimentary json proxy
-         * of an xml endpoint.
-         *
-         * @param rawObject the object retrieved from the server
-         */
-        parse: function(rawObject) {
-            var parsedObject = {};
-            parsedObject.id = parseInt(rawObject.$.objectid, 10);
-            parsedObject.name = rawObject.name[0]._;
-            parsedObject.yearPublished = parseInt(rawObject.yearpublished[0], 10);
-            parsedObject.image = rawObject.image[0];
-            parsedObject.thumbnail = rawObject.thumbnail[0];
-            parsedObject.minPlayers = parseInt(rawObject.stats[0].$.minplayers, 10);
-            parsedObject.maxPlayers =  parseInt(rawObject.stats[0].$.maxplayers, 10);
-            parsedObject.playingTime = parseInt(rawObject.stats[0].$.playingtime, 10);
-            parsedObject.userRating = parseFloat(rawObject.stats[0].rating[0].$.value);
-            parsedObject.averageRating = parseFloat(rawObject.stats[0].rating[0].average[0].$.value);
-            return parsedObject;
-        },
-
         isOlderThan: function(age) {
             var currentYear = new Date().getFullYear();
             var minimumYear = currentYear - age;
