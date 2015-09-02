13f23eda089e4cf39ff40151812562ae2aacafc0
Jim Sullivan <jimpatricksullivan@gmail.com>
2015-09-01 22:06:02 -0400

    Finish tests for BggApiUtil

--- spec/BggApiUtil.spec.js
+++ spec/BggApiUtil.spec.js
@@ -1,16 +1,16 @@
-/*global require, module, describe, beforeEach, it */
+/* global require, module, describe, beforeEach, it, afterEach */
 var chai = require("chai");
 var sinon = require("sinon");
 var when = require('when');
 var bggApiUtil = require("../src/BggApiUtil");
 
-var fakeXml = '' +
+var fakeResponse = '' +
     '<items>' +
     '   <item objecttype="thing" objectid="1" subtype="boardgame" collid="16402370">' +
     '       <name sortindex="1">Dusseldorf</name>' +
     '       <yearpublished>2014</yearpublished>' +
-    '       <image>//cf.geekdo-images.com/images/pic1652300.png</image>' +
-    '       <thumbnail>//cf.geekdo-images.com/images/pic1652300_t.png</thumbnail> ' +
+    '       <image>//cf.geekdo-images.com/images/dorf.png</image>' +
+    '       <thumbnail>//cf.geekdo-images.com/images/dorf_t.png</thumbnail> ' +
     '       <stats minplayers="2" maxplayers="4" minplaytime="15" maxplaytime="45" playingtime="45" numowned="1414"> ' +
     '           <rating value="8"> ' +
     '               <usersrated value="1130"/> ' +
@@ -29,8 +29,8 @@ var fakeXml = '' +
     '   <item objecttype="thing" objectid="2" subtype="boardgame" collid="16402370">' +
     '       <name sortindex="1">Xeno Shooter: Reckoning Alpha</name>' +
     '       <yearpublished>2015</yearpublished>' +
-    '       <image>//cf.geekdo-images.com/images/pic1652300.png</image>' +
-    '       <thumbnail>//cf.geekdo-images.com/images/pic1652300_t.png</thumbnail> ' +
+    '       <image>//cf.geekdo-images.com/images/xeno.png</image>' +
+    '       <thumbnail>//cf.geekdo-images.com/images/xeno_t.png</thumbnail> ' +
     '       <stats minplayers="1" maxplayers="6" minplaytime="120" maxplaytime="240" playingtime="240" numowned="1000"> ' +
     '           <rating value="7"> ' +
     '               <usersrated value="1130"/> ' +
@@ -48,19 +48,61 @@ var fakeXml = '' +
     '   </item>' +
     '</items>';
 
+var expectedResult = [{
+        "id": 1,
+        "name": "Dusseldorf",
+        "yearPublished": 2014,
+        "image": "//cf.geekdo-images.com/images/dorf.png",
+        "thumbnail": "//cf.geekdo-images.com/images/dorf_t.png",
+        "minPlayers": 2,
+        "maxPlayers": 4,
+        "playingTime": 45,
+        "userRating": 8,
+        "averageRating": 6.55738
+    }, {
+        "id": 2,
+        "name": "Xeno Shooter: Reckoning Alpha",
+        "yearPublished": 2015,
+        "image": "//cf.geekdo-images.com/images/xeno.png",
+        "thumbnail": "//cf.geekdo-images.com/images/xeno_t.png",
+        "minPlayers": 1,
+        "maxPlayers": 6,
+        "playingTime": 240,
+        "userRating": 7,
+        "averageRating": 6.55738
+    }];
+
 describe('BggApiUtil', function(){
+    var sandbox, requestStub;
+
     beforeEach(function (){
-        sinon.stub(bggApiUtil, '_request').returns({
-            statusCode: 200,
-            body: fakeXml
-        });
-        sinon.stub(bggApiUtil, '_pause').returns(when.resolve());
+        sandbox = sinon.sandbox.create();
+        sandbox.stub(bggApiUtil, '_pause').returns(when.resolve());
+        requestStub = sandbox.stub(bggApiUtil, '_request');
+    });
+
+    afterEach(function() {
+        sandbox.restore();
     });
 
     it('parses a board game collection', function *() {
+        requestStub.returns({
+            statusCode: 200,
+            body: fakeResponse
+        });
         var results = yield bggApiUtil.getGameCollection('nobody');
         chai.expect(bggApiUtil._pause.called).to.not.be.ok;
-        chai.expect(results.body.length).to.equal(2);
-        //TODO expect more stuff
+        chai.expect(results.body).to.eql(expectedResult);
+    });
+
+    it('calls bgg multiple times if it returns 202s', function *() {
+        requestStub.onCall(0).returns({ statusCode: 202 });
+        requestStub.onCall(1).returns({ statusCode: 202 });
+        requestStub.onCall(2).returns({
+            statusCode: 200,
+            body: fakeResponse
+        });
+        yield bggApiUtil.getGameCollection('nobody');
+        chai.expect(bggApiUtil._pause.callCount).to.equal(2);
     });
 });
