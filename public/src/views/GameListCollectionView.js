ad51c73f869d728762934db8b220526529395e22
Jim Sullivan <jimpatricksullivan@gmail.com>
2015-10-08 07:50:35 -0400

    ultra-rudimentary masonry layout but hey at least it's working.

--- public/src/views/GameListCollectionView.js
+++ public/src/views/GameListCollectionView.js
@@ -1,8 +1,21 @@
 define([
     'marionette',
-    'views/GameListItemView'
-], function (Marionette, GameListItemView) {
+    'views/GameListItemView',
+    'bower_components/masonry/dist/masonry.pkgd.js'
+], function (Marionette, GameListItemView, Masonry) {
     return Marionette.CollectionView.extend({
-        childView: GameListItemView
+        childView: GameListItemView,
+
+        onShow: function() {
+
+            var el =this.$el.get(0);
+
+            setTimeout(function() {
+                window.masonry = new Masonry( el, {
+                    columnWidth: 160
+                });
+            }, 2000);
+
+        }
     });
 });
